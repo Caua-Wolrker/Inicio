a595cfa77389fb89bcda594cf8b618d627cba087
Wolrker <caua.saraiva.freires@gmail.com>
2023-04-18 15:04:14 -0300

    Inicio

---
+++ script.js
@@ -0,0 +1,9 @@
+window.alert("Eae");
+
+
+
+
+if (window.confirm("Antes de você continuar,eu quero fazer uma pergunta, pode ser?") == true) {
+    window.prompt("Como você achou o meu site?");
+    alert("Obrigado pela ajuda!")
+}
