955f9a68b930b91ca63add167c17615ae366cb59
Caua-Wolrker <112732963+Caua-Wolrker@users.noreply.github.com>
2023-04-27 15:41:26 -0300

    Atualização braba dms

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
+
+/*
 window.alert("Eae");
-
-
-
 
 if (window.confirm("Antes de você continuar,eu quero fazer uma pergunta, pode ser?") == true) {
     window.prompt("Como você achou o meu site?");
     alert("Obrigado pela ajuda!")
+}
+*/
+
+window.onresize = function() {
+    var areasLinguagens = documento.getElementById("areasLinguagens");
+    var larguraDaJanela = window.innerWidth;
+    var posicaoDaDiv = areasLinguagens.getBoundingClientRect().right;
+
+    if (posicaoDaDiv >= larguraDaJanela) 
+    {
+        areasLinguagens.classList.add("diminuir");
+    } else {
+        areasLinguagens.classList.remove("diminuir");
+    }
 }
