071a36dbbdf7d3ee5186d7d8b348b927096bba0f
Caua-Wolrker <112732963+Caua-Wolrker@users.noreply.github.com>
2023-05-02 17:01:41 -0300

    Atualização menu

--- script.js
+++ script.js
@@ -10,15 +10,17 @@ if (window.confirm("Antes de você continuar,eu quero fazer uma pergunta, pode s
 
 function barraMenu() {
     var camadaM = document.getElementById("camadaM");
-    var menuText = document.getElementById("menuText");
+    var sombra = document.getElementById("sombra");
 
     if (camadaM.style.left == "-200px") 
     {
+        sombra.style.opacity = "50%"
         camadaM.style.left = "0";
         camadaM.style.width = "200px";
         camadaM.style.height = "100%";
     } else
     {
+        sombra.style.opacity = "0"
         camadaM.style.left = "-200px";
         camadaM.style.width = "0";
         camadaM.style.height = "0";
